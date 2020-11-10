7067b513c46e9cf80f01ccf773bbebd39d83ec0f
saki10 <rsp10111011@gmail.com>
2020-11-10 19:57:25 +0900

    会員画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'pages/index'
   get 'pages/show'
   get 'users/edit'
+  get 'users/kanin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
